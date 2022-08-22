9d276eb0f77b96a3dc3cb981cd9dc98c827feb29
Omar Al Raisi <o.raisi@hotmail.com>
2022-08-22 22:47:58 +0400

    Basic UI complete
    Note:
    The design does not look good at all, but focus on the code for now and work on the design while coding if any idea pops up, otherwise keep it like this until everything is done.
    
    Next:
      i. Write the basic class for the view controllers.
     ii. Handle the key (DM5 encryption).
    iii. Fetch and parse the data of the characters.
    iv. Fetch and parse the data of the comics.
    
    A plus: Clean the code and look for performance improvements.

---
+++ PhazeRo-Marvel/Controllers/CharacterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CharacterViewController.swift
+//  PhazeRo-Marvel
+//
+//  Created by Omar Al Raisi on 22/08/2022.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
